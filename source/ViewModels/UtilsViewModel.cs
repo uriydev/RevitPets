a3e265cb4b87c745f75e10067522986cd95c4511
uriy <78085017+monstebit@users.noreply.github.com>
2025-02-14 22:39:34 +0300

    edit VM

--- source/ViewModels/UtilsViewModel.cs
+++ source/ViewModels/UtilsViewModel.cs
@@ -1,13 +1,26 @@
-using System.Windows.Threading;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 using Nice3point.Revit.Toolkit.External.Handlers;
 using RevitPets.Views.Utils;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace RevitPets.ViewModels
 {
     public partial class UtilsViewModel : ObservableObject
     {
+        private double _panelWidth;
+
+        public double PanelWidth
+        {
+            get => _panelWidth;
+            set => SetProperty(ref _panelWidth, value);
+        }
+        
         [ObservableProperty]
-        private bool _isWalking;
+        private bool isWalking;
 
         [ObservableProperty]
         private string animatedSource;
@@ -15,92 +28,47 @@ namespace RevitPets.ViewModels
         [ObservableProperty]
         private double xOffset;  // Смещение по X
 
-        private double walkSpeed = 1;  // Скорость движения
-        private DispatcherTimer walkTimer;
+        private readonly double walkSpeed = 1;  // Скорость движения
 
         public UtilsViewModel()
         {
-            AsyncEventHandler = new AsyncEventHandler();
+            RibbonController.SubscribeToSizeChanged(this);
             
-            AnimatedSource = "pack://application:,,,/RevitPets;component/Resources/Animations/Anim399.gif";  // Стоять
-
-            // Инициализация таймера для плавного обновления
-            walkTimer = new DispatcherTimer
-            {
-                Interval = TimeSpan.FromMilliseconds(1) // Обновление каждую 10-ую миллисекунду
-            };
-            walkTimer.Tick += OnWalkTimerTick;
+            // Начальная анимация - кот стоит
+            AnimatedSource = "pack://application:,,,/RevitPets;component/Resources/Animations/Idle.gif";
         }
         
-        public AsyncEventHandler AsyncEventHandler { get; }
-
-        // Команда для переключения состояния (используем атрибут [RelayCommand])
         [RelayCommand]
         private void ToggleWalking()
         {
             IsWalking = !IsWalking;
 
-            // Переключение анимации
             AnimatedSource = IsWalking
-                ? "pack://application:,,,/RevitPets;component/Resources/Animations/Anim193x200.gif"  // Бег
-                : "pack://application:,,,/RevitPets;component/Resources/Animations/Anim399.gif";      // Стоять
+                ? "pack://application:,,,/RevitPets;component/Resources/Animations/RunRight.gif"
+                : "pack://application:,,,/RevitPets;component/Resources/Animations/Idle.gif";
 
-            // Запуск или остановка движения
             if (IsWalking)
                 StartWalking();
-            else
-                StopWalking();
         }
 
-        // private void StartWalking()
-        // {
-        //     // Запуск таймера для анимации смещения
-        //     walkTimer.Start();
-        // }
-        
-        // Метод для запуска движения
-        public async Task StartWalking()
+        private async Task StartWalking()
         {
-            IsWalking = true; // Начинаем движение
-
-            // Запускаем таймер на 5 секунд
             var stopTime = DateTime.Now.AddSeconds(5);
 
             while (IsWalking && DateTime.Now < stopTime)
             {
-                // Двигаем кота вправо
                 XOffset += walkSpeed;
 
-                // Если кот выходит за пределы экрана, сбрасываем его позицию
                 if (XOffset > 300)
                     XOffset = 0;
 
-                // Плавная задержка для анимации
-                await Task.Delay(10); // ~100 FPS
+                await Task.Delay(10);
             }
 
-            IsWalking = false; // Останавливаем движение
+            IsWalking = false;
+            AnimatedSource = "pack://application:,,,/RevitPets;component/Resources/Animations/Idle.gif";
         }
 
-        private void StopWalking()
-        {
-            // Останавливаем таймер и сбрасываем позицию
-            walkTimer.Stop();
-            XOffset = 0;
-        }
-
-        // Метод для обновления смещения
-        private void OnWalkTimerTick(object sender, EventArgs e)
-        {
-            // Двигаем кота вправо
-            XOffset += walkSpeed;
-
-            // Если кот выходит за пределы экрана, сбрасываем его позицию
-            if (XOffset > 300)
-                XOffset = 0;
-        }
-
-        // Команда для удаления панели опций
         [RelayCommand]
         private void RemoveOptionsBar()
         {
